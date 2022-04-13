502c545929e10e5cd6011876f91bd265b7f8079c
emmyzon <e.wolchko@gmail.com>
2022-04-13 17:15:51 -0400

    parallax progress

---
+++ Harvard_Action3/Assets/Sprites/TempArt/emily/BG_Parallax.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class BG_Parallax : MonoBehaviour {
+
+      private float length, startposX;
+      //startposY;
+      public GameObject cam;
+      public float parallaxEffect;
+      //parallaxUpEffect;
+
+      void Start(){
+            startposX = transform.position.x;
+            //startposY = transform.position.y;
+            length = GetComponent<SpriteRenderer>().bounds.size.x;
+      }
+
+      void FixedUpdate(){
+            float temp = (cam.transform.position.x * (1 - parallaxEffect));
+            float distX = (cam.transform.position.x * parallaxEffect);
+            //float distY = (cam.transform.position.y * parallaxUpEffect);
+            transform.position = new Vector3(startposX + distX, transform.position.y, transform.position.z);
+            //transform.position = new Vector3(startposX + distX, startposY + distY, 1);
+            if (temp > startposX + length){
+                  startposX += length;
+            }
+            else if (temp < startposX - length){
+                  startposX -= length;
+            }
+      }
+
+}
