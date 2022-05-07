4ff5e9093736c9f84b0c8a6640b6033eb4224a2f
Stu <stuartmvg@gmail.com>
2022-05-07 16:36:31 -0400

    Revert "Merge branch 'Stu_Volume_Control'"
    This reverts commit 7a51c50bab18eb1590c95e738a04e1594d1cce32, reversing
    changes made to 260783736f69df5f3b837a4da75c6eec9580bafe.

--- Harvard_Action3/Assets/Scripts/Controls/AudioMasterControl.cs
+++ Harvard_Action3/Assets/Scripts/Controls/AudioMasterControl.cs
@@ -8,61 +8,21 @@ namespace Game.Control
 {
     public class AudioMasterControl : MonoBehaviour 
     {
-            [SerializeField] AudioMixer musicMixer;
-            [SerializeField] Slider musicSliderVolumeCtrl;
+            [SerializeField] AudioMixer mixer;
+            [SerializeField] Slider sliderVolumeCtrl;
 
-            [SerializeField] AudioMixer sFXMixer;
-            [SerializeField] Slider sFXSliderVolumeCtrl;
-
-            private VolumeSingleton volumeSingleton;
-            private AudioSource levelMusicSource;
+            public static float volumeLevel = 0.2f;
 
             private void Awake ()
             {
-                volumeSingleton = GameObject.FindWithTag(Tags.VOLUME_STATE_PERSISTS_TAG).GetComponent<VolumeSingleton>();
-                levelMusicSource = GameObject.FindWithTag(Tags.LEVEL_MUSIC_TAG).GetComponent<AudioSource>();
+                SetLevel(volumeLevel);
             }
 
-            private void OnEnable()
+            public void SetLevel(float sliderValue)
             {
-                EventHandler.ActiveGameUI += ToggleLevelMusic;
-            }
-
-            private void OnDisable()
-            {
-                EventHandler.ActiveGameUI -= ToggleLevelMusic;
-            }
-
-            private void Start() 
-            {
-                SetMusicLevel(volumeSingleton.GetMusicVolumeLevel());
-                SetSFXLevel(volumeSingleton.GetSXFVolumeLevel());
-            }
-
-            public void SetMusicLevel (float sliderValue)
-            {
-                volumeSingleton.SetMusicVolumeLevel(sliderValue);
-                musicMixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
-                musicSliderVolumeCtrl.value = sliderValue;
-            }
-            
-            public void SetSFXLevel (float sliderValue)
-            {
-                volumeSingleton.SetSFXVolumeLevel(sliderValue);
-                sFXMixer.SetFloat("SFXVolume", Mathf.Log10 (sliderValue) * 20);
-                sFXSliderVolumeCtrl.value = sliderValue;
-            }
-
-            private void ToggleLevelMusic(bool isGamePaused)
-            {
-                if (levelMusicSource.isPlaying)
-                {
-                    levelMusicSource.Pause();
-                }
-                else
-                {
-                    levelMusicSource.Play();
-                }
+  
+                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+                sliderVolumeCtrl.value = sliderValue;
             }
     }
 }
--- Harvard_Action3/Assets/Scripts/Utils/Tags.cs
+++ Harvard_Action3/Assets/Scripts/Utils/Tags.cs
@@ -4,10 +4,6 @@ public static class Tags
     public const string PARTICLE_CONTAINER_TAG = "ParticleContainer";
     public const string SOLID_CONTAINER_TAG = "SolidContainer";
     public const string WILD_PARTICLE_TAG = "WildParticle";
-
-    public const string VOLUME_STATE_PERSISTS_TAG = "VolumePersists";
-    public const string LEVEL_MUSIC_TAG = "LevelMusic";
-
     
 
 }
